966f9827fcbdca2e9fa2acceb2ddd6c94e70aedb
elmersonse <pkititsa200@gmail.com>
2022-09-07 15:51:11 +0300

    fixes

--- CryptoTracker/Startup.cs
+++ CryptoTracker/Startup.cs
@@ -77,7 +77,7 @@ namespace CryptoTracker
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            // app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
             app.UseDefaultFiles();
             app.UseStaticFiles();
 
